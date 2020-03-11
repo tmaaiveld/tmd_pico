aead4e0351c5304c9e14271fdfa3df1a2c9d4a73
tmaaiveld <35788671+tmaaiveld@users.noreply.github.com>
2020-03-11 02:36:16 +0100

    @Sanne: uploaded latest version of corpus @Soufyan: added sentenced data, example to load in Sentence Parsing notebook

--- corpus.py
+++ corpus.py
@@ -3,128 +3,57 @@ import decimal as dec
 from nltk.stem.snowball import SnowballStemmer
 
 class Corpus: # move later
-    """A class which receives a pd.Series of indexed documents and can execute various preprocessing methods to produce different version of word columns or different features to be added."""
+    """
+    A class which receives a pd.Series of token strings indexed by document. Attributes include a doc_series
+    which retains the unchanged time series text data, a df to which new features may be appended. The object can be
+    loaded in any module by passing a loaded document series and d can execute various preprocessing methods to
+    produce different version of word columns or different features to be added.
+    """
 
-    # add methods to create processing protocols for data
-    # lowercasing
+    # todo: add a method for detecting stop words
 
-    # special chars are best left unhandled as they are strong predictors
-    # could replace uncommon chars completely, at least for the W2V columns.
-    # any preprocessing that doesn't remove instances is fine, and can be used to generate more features
+
 
     # https://radimrehurek.com/gensim/parsing/preprocessing.html
-    # https://www.nltk.org/_modules/nltk/parse/stanford.html <- can try stanford again using sep sent tokens?
+    # https://www.nltk.org/_modules/nltk/parse/stanford.html
 
     def __init__(self, doc_series):
         self.doc_series = doc_series # remains unchanged
-        self.df = doc_series.to_frame() # is changed
-        self.idx = doc_series.index
+        # self.idx = doc_series.index
+        self.df = doc_series.to_frame() # is changed by corpus methods
         self.doc_lists = doc_series.groupby('doc').apply(list).tolist()
+
         self.sent_lists = None
-        
-        print(self.df)
-        quit()
-
-    # modify series.name as
-    
-    def return_words_nparr(self):
-        return self.df.values.tolist()
-        
-    def process(self):
+        self.sent_index = None
         self.idx_sentences()
-        self.lower()
 
-
-    def idx_sentences(self): # todo: could be cleaned up
-        df = self.df
-
-        sent_break = df['Word'].mask(df['Word'] == '.', 1).mask(df['Word'] != '.', 0)  \
-                               .droplevel(level='idx').fillna(method='bfill')
-
-        sent_id = sent_break.groupby('doc').shift(1).fillna(method='bfill')  \
-                            .groupby('doc').expanding().sum()                \
-                            .astype(int).rename('sent_id')
-
-        self.df['sent_id'] = sent_id.values
-        self.df = self.df.set_index('sent_id', append=True).reset_index(level='idx')
-
-        # self.df['sent_id'] = sent_id.values # set to int for now
-
-        print(sent_id.values)
-        print(self.df)
-        quit()
-
-
-        sent_pos = pd.Series(1, index=df.index).groupby(['doc','sent_id']).expanding().sum()
-
-        # print(df)
-        # sent_pos = df['dummy']#.groupby(['doc','sent_id']).expanding().sum()
-        print(sent_pos)
-        quit()
-
-        self.sentences = df['Word'].groupby(['doc','sent_id']).apply(list)
-
-        self.df['sent_pos'] = sent_pos.reset_index(drop=True)
-
-        # print(sent_pos)
-        # self.sentences = sent_pos
-                                         # .droplevel(level='sent_id').set_index('idx', append=True)
-
-
-
-        # print(self.sentences)
-
-
-        # todo: test
-
-        # print(df)
-
-        return self
-
-
-    def by_sentence(self):
-        try:
-            self.df = self.df.set_index('sent_id', append=True).reset_index(level='idx')
-
-        except KeyError:
-            print('sent_id index not found. Attempting to index sentences.')
-            self.idx_sentences()
-
-        return self
-
-
-
-    def sent_pos(self):
-        self.df['sent_pos'] = self.sentences.groupby('doc', 'sent_id').expanding().sum()
-
-
-    def lower(self): # todo: test
+    def lower(self): # todo: untested
 
         self.df['Word'] = [word.lower() for word in self.df['Word']]
 
         return self
 
-    def stem(self):
+    def stem(self): # todo: untested
+        stemmer = SnowballStemmer("english", ignore_stopwords=True)
+        self.df['Word'] = [stemmer.stem(word) for word in self.df['Word']]
 
-        self.df['Word'] = [SnowballStemmer("english", ignore_stopwords=True).stem(word)
-                           for word in self.df['Word']]
         return self
 
     def replace_num(self):
         """
-        Check if a token is numeric. Numbers with units and a-mods ('2-fold') are not changed.
-        Full numeric ('25') is handled differently from decimal ('-0.1'). This is to distinguish between counts and
-        measurements/variables/hyperparameters in the text.
+        Currently doesn't work.
         """
-        self.df['Words'] = self.df['Words'].apply(self.handle_numeric)
+        raise NotImplementedError('handle_numeric is not working')
+        # .apply applies a function to every item of the dataframe.
+        self.df['Word'] = self.df['Word'].apply(self.handle_numeric).values
 
         return self
 
+    # @staticmethod tells Python a method is only placed in the class for readability purposes.
     @staticmethod
     def handle_numeric(lemma):
         if lemma.isnumeric():
             return '<NUM>'
-
         else:
             try:
                 dec.Decimal(lemma)
@@ -132,38 +61,54 @@ class Corpus: # move later
             except dec.InvalidOperation:
                 return lemma
 
-    # preprocessing by method chaining
-    # see deprecated for some suggestions
+    def idx_sentences(self):
 
-    # write methods for...
-        # lowercase
-        # special char removal (improves POS)
-        # pos tagging? or keep in pipeline?
-        # stemming
-        # numchar replace (see depr)
+        # First, slice the word column from the Corpus.df. .to_frame() converts that column to a new DataFrame.
+        new_indices = self.df['Word'].copy().to_frame()
 
-    # try deprel with Stanford once again (use nltk.parse.stanford.parse_sents
+        # this code calculates the sentence ID number for each token and adds it to the new_indices DataFrame.
+        sent_break = new_indices.mask(new_indices == '.', 1)                     \
+                                .mask(new_indices != '.', 0)                     \
+                                .droplevel(level='idx')                          \
+                                .fillna(method='bfill')
 
-    # pos_replace = {
-    #     '$': 'SYM',
-    #     '#': 'SYM',
-    # }
-    # features['POS'] = features['POS_NLTK'].replace(['$', '#'], 'SYM')
+        sent_id = sent_break.groupby('doc').shift(1).fillna(method='bfill')      \
+                            .groupby('doc').expanding().sum().astype(int).reset_index(drop=True)
 
-    # could test other postaggers if they can tag a list
-    # add nltk tnt pos tag < tests
-    # add gensim pos tag < tests
+        new_indices['sent'] = sent_id.values + 1
+        new_indices['token'] = 1
+        new_indices = new_indices.set_index('sent', append=True).reorder_levels(['doc','sent','idx'])
 
+        new_indices['token'] = new_indices['token'].droplevel(level='idx')                      \
+                                                   .groupby(['doc', 'sent']).expanding().sum()  \
+                                                   .astype(int).values
 
-    def zip_docs(self):
-        self.df.groupby('doc').apply(list).to_dict() # list of lists output for zip methods
+        new_indices = new_indices.reset_index('sent').drop('Word', axis=1)
+
+        # The resulting index is saved separately.
+        self.sent_index = new_indices.set_index(['sent', 'token'], append=True) \
+                                     .reset_index('idx', drop=True).index
+
+        # Last, we return self to allow this method to be chained with other methods of the Corpus object.
+        return self
+
+    def by_sent(self):
+
+        self.df.index = self.sent_index
+        return self
+
+    def loc_features(self):
+
+        new_indices = pd.DataFrame(index=self.sent_index).reset_index(['sent', 'token']).astype(str)
+        self.df['sent_loc']  = new_indices['sent'].values
+        self.df['token_loc'] = new_indices['token'].values
+        return self
 
     def zip_sents(self):
-        self.df.groupby('doc', 'sentence').apply(list).to_dict()
-
-    # def __iter__(self): # todo: fix or remove
-    #     for line in open(self.pkl): # see function below for file building
-    #         yield utils.simple_preprocess(line) # doesn't work yet because of stop word removal etc.
+        # check if data is correctly formatted?
+        docs = self.doc_series
+        docs.index = self.sent_index
+        return docs.groupby(['doc', 'sent']).apply(list).to_dict()
 
 
 if __name__ == '__main__':
@@ -173,25 +118,39 @@ if __name__ == '__main__':
     # could use parent/child (DocSeries parent, Corpus child)
     print('loading data')
     data = pd.read_parquet('data/split/train_1000.parquet')['Word']
-    corp = Corpus(data[1000:])
+    corp = Corpus(data)
 
-    print(corp)
+    # index the corpus by sentence
+    corp.by_sent()
+    print(corp.df)
 
-    num_replace = Corpus(data).replace_num() # Important, but may already work
-    print(num_replace)
-    
-    sent_tagged = corp.idx_sentences().df
-    print(sent_tagged)
+    # add word/sent location features to df
+    corp.loc_features()
+    print(corp.df)
 
+    # zip the sentences to a dict of lists for parsing
+    zip_sents = corp.zip_sents()
+    print(zip_sents)
+
+    # save the zipped sentences to a list.
+    import pickle
+    with open('data/sentences.pickle', 'wb') as handle:
+        pickle.dump(zip_sents, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # detecting stop words
+    raise NotImplementedError()
+
+    # Stemming. Seems to work ok.
+    stemmed = Corpus(data).stem().df
+    print(stemmed)
+
+    # Numerical char replacement. Doesn't work.
+    num_replace = Corpus(data).replace_num().df
+    print(num_replace.head(100))
+
+    # lowercasing. Seems to work.
     lowercased = Corpus(data).lower().df
     print(lowercased)
 
-    stemmed = Corpus(data).stem()
-    print(stemmed)
 
-
-
-    print(pd.concat([sent_tagged, lowercased, stemmed, num_replace], axis=1))
-
-
-
+    # print(pd.concat([sent_tagged, lowercased, stemmed, num_replace], axis=1))
